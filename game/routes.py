33bf98b6f426c16dbdd343c1b51e54db02d9891a
sz <surazgyawali@gmail.com>
2018-04-14 14:20:09 +0545

    init - static file route moved to init

--- game/routes.py
+++ game/routes.py
@@ -8,12 +8,6 @@ from flask import render_template
 from game import app
 
 
-
-@app.route('/static/<path:filepath>')
-def serve_static(filename):
-    return flask.url_for('static', filename)
-
-
 @app.route('/')
 @app.route('/index')
 def index():
