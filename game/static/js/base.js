0ecc06a648fee6431845636616158d9c73c7a4e9
sz <surazgyawali@gmail.com>
2018-04-27 03:28:37 +0545

    js update

--- game/static/js/base.js
+++ game/static/js/base.js
@@ -1,3 +1,27 @@
 function gameStart(){
     alert("The game is going to start!")
+
+$('#next').click(function(){
+    $('#welcome').addClass("d-none")
+    $('#intro').removeClass("d-none")
+})
+
+$('#gameStart').click(function () {
+    ajax_fetch()
+})
+
+function ajax_fetch() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = this.responseText;
+            $('html').html(response);
+        }
+    };
+    xhttp.open(
+        method = "POST",
+        url = "/game",
+        true
+    );
+    xhttp.send();
 }
