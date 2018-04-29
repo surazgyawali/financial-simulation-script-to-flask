f9ca038ddb401d468eba8a169af1ebb033d3ac12
sz <surazgyawali@gmail.com>
2018-04-30 01:45:19 +0545

    routes for action 1 and 2 updated according to new methods

--- game/routes.py
+++ game/routes.py
@@ -17,8 +17,7 @@ def send_post_request():
                 flask.request.url_root +'api/game',
             )
         except requests.exceptions.RequestException as e:
-            return e
-
+            return redirect('/restart')
         jsonData = response.json()
         flask.session['sessionData'] = jsonData
 
@@ -28,7 +27,7 @@ def send_post_request():
             headers = {'sessid': flask.session['sessionData']['sessid']}
         )
     except requests.exceptions.RequestException as e:
-        return e
+        return redirect('/restart')
     jsonData = response.json()
     return jsonData
 
@@ -82,7 +81,6 @@ def gameLoopQuestions():
 @app.route('/')
 @app.route('/index')
 def index():
-    flask.session.clear()
     return flask.render_template(
         'welcome.djhtml',
         header   = 'Greetings!!!',
@@ -98,76 +96,50 @@ def index():
         id       = 'gameStart'
     )
 
-@app.route('/main', methods = ['GET'])
-def introPage():
-    #To Do, put buyers and securitization buyers in array
-    return flask.render_template(
-        'game.djhtml',
-        header   = "Hi There!!!",
-        messages = [
-                    "You are the CEO at a small start up lending platform, 'BestLending Inc.'",
-                    "Your job is to ensure the growth of the platform to profitability.",
-                    "Do this by managing the speed you grow the platform and choosing the best method of funding to minimize your company's Weighted Average Cost of Capital (WACC)."
-                ],
-        blabel   = "Next >>",
-    )
-
 @app.route('/game',methods = ['GET','POST'])
 def gameStart():
     if request.method == 'POST':
-        if flask.session.get('sessionData') is None:
-            data = requests.post(
-                flask.request.url_root +'api/game',
-            )
-            jsonData = data.json()
-            flask.session['sessionData'] = jsonData
-
-
-        data = requests.post(
-            flask.request.url_root +'api/game',
-            headers = {'sessid': flask.session['sessionData']['sessid']}
-        )
-        jsonData = data.json()
-        flask.session['sessionData'] = jsonData
+        jsonData = send_post_request()
         messages = jsonData['data']
-        sessid = jsonData['sessid']
-        return flask.render_template(
+        return render_template(
             'game.djhtml',
             header     = "On your command.",
             # messages  = messages[0:3],
             stats      = messages[3:12],
-            options    = messages[12:len(messages)-1],
-            question   = messages[len(messages)-1],
-            uri        = '/game',
-            field_name = 'main',
+            **gameLoopQuestions()
         )
 
-    if request.method == 'GET':
+    else:
         response = 0
         response = request.args.get('main')
-        headerData = {
-            'sessid'  : flask.session['sessionData']['sessid'],
-            'message' : response
-        }
-        print(flask.session['sessionData']['sessid'])
-        print(headerData)
         response = int(response)
         while response != 12:
             if response == 1:
-                jResponse = requests.get(
-                    flask.request.url_root +'api/game',
-                    headers = headerData
-                ).json()
+                jResponse = send_get_request('1')
                 messages = jResponse['data']
                 return flask.render_template(
-                    'game.djhtml',
+                    'hireFire.djhtml',
                     header     = "“Great vision without great people is irrelevant.",
-                    messages   = messages + ['Be careful: You have  to maintain the rate of contract as well as hire i.e the value could only be between -5 to 10.'],
-                    options    = [i for i in range(-5,11)],
+                    messages   = [messages[1]],
+                    stats      = [messages[0]],
                     question   = "The moment has come.",
+                    uri        = 'game/1',
+                    field_name = 'hire'
                 )
+
             elif response == 2:
-                return "TODO: The task."
+                jResponse = send_get_request('2')
+                messages=jResponse['data']
+                return flask.render_template(
+                    'game.djhtml',
+                    header     = "Platform Income Interest",
+                    stats      = messages[0:9],
+                    messages   = [messages[9]],
+                    options    = messages[10:len(messages)-1],
+                    question   = messages[len(messages)-1],
+                    uri        = '/game',
+                    field_name = 'main',
+                )
             elif response == 3:
                 return "TODO: The task."
             elif response == 4:
@@ -187,9 +159,15 @@ def gameStart():
             elif response == 11:
                 return "TODO: The task."
             elif response == 13:
-                flask.flash('You sucessfully retired!!!',category='success')
-                flask.session.clear()
-                return redirect(url_for('.index'))@app.route('/restart')
+                # jResponse = send_get_request('13')
+                return flask.redirect('/restart')
+
+@app.route('/game/1')
+def game_hire():
+    response = None
+    response = request.args.get('hire')
+    jsonData = send_get_request(response)
+    return game_loop("Some RecruitMent has been done.Keep Going.")
 
 @app.route('/restart')
 def restart():
