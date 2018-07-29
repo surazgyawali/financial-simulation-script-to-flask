3ee5f75e94eb579495b496ff0dfb768bc05298a0
sz <surazgyawali@gmail.com>
2018-07-29 23:02:08 +0545

    Squashed commit of the following:
    commit 3474d45e25bcc5aeee7a34582553717f0017b939
    Author: sz <surazgyawali@gmail.com>
    Date:   Sun Jul 29 22:53:55 2018 +0545
    
        debug - sessid fix
    
    commit 5163888c03bae37a633eb41e18319643f3efc8da
    Author: sz <surazgyawali@gmail.com>
    Date:   Sun Jul 29 22:46:12 2018 +0545
    
        debug - fix sessid scope
    
    commit 2e7efb31ae933a2c8c7dc89377cea6263ce06105
    Author: sz <surazgyawali@gmail.com>
    Date:   Sun Jul 29 22:17:40 2018 +0545
    
        debug
    
    commit 66af12c8ff00b30f884674a923009786c2fd4ad1
    Author: sz <surazgyawali@gmail.com>
    Date:   Sun Jul 29 10:34:56 2018 +0545
    
        debug
    
    commit 5b8a7e6dffd42fc556a5a3c270eeb48dea599363
    Author: sz <surazgyawali@gmail.com>
    Date:   Sat Jul 28 21:04:31 2018 +0545
    
        heroku - debug test
    
    commit 52bc435f568feebefbbf45e3cdc66a5a65496ae2
    Author: sz <surazgyawali@gmail.com>
    Date:   Sat Jul 28 20:17:21 2018 +0545
    
        templates -  autofocus and validation fix
    
    commit 10031dc9991197eadc15dc0f9473b03ad27e006a
    Author: sz <surazgyawali@gmail.com>
    Date:   Sat Jul 28 20:16:12 2018 +0545
    
        ctrl - session id consitent fix
    
    commit dfcf44840026d7fdee59637e2b4b37106853b991
    Author: sz <surazgyawali@gmail.com>
    Date:   Sat Jul 28 20:13:00 2018 +0545
    
        refactor module rename
    
    commit d747179aedaeb8bf588135034260517fe7fbddc4
    Author: sz <surazgyawali@gmail.com>
    Date:   Fri Jul 27 01:40:41 2018 +0545
    
        refactor - indetation consitency fix
    
    commit a8098062324e7e9df4a47328c47ba46d9c267ac2
    Author: sz <surazgyawali@gmail.com>
    Date:   Mon Jun 4 01:19:41 2018 +0545
    
        debug
    
    commit 2b3d3ed1e9775c0554117e033246902eebc92cb7
    Author: sz <surazgyawali@gmail.com>
    Date:   Mon Jun 4 01:10:03 2018 +0545
    
        debug - sessid visibile everywhere
    
    commit f3baf1d522ee54e5eb30127482144fa23ff5aefc
    Author: sz <surazgyawali@gmail.com>
    Date:   Sun Jun 3 21:28:36 2018 +0545
    
        fix - reset session on error
    
    commit 3adfba0e5744db3777f2ebe0c21caebe48f42ecc
    Author: sz <surazgyawali@gmail.com>
    Date:   Sun Jun 3 21:04:37 2018 +0545
    
        missing separation in main fix
    
    commit 7a9ecf4a5391d81abad87edc8addec278dd189c5
    Author: sz <surazgyawali@gmail.com>
    Date:   Sun Jun 3 20:02:09 2018 +0545
    
        request logic and display routes separated
    
    commit eac98a67c76e62799540f7dc44dd4c004e968c69
    Author: sz <surazgyawali@gmail.com>
    Date:   Sun Jun 3 20:01:30 2018 +0545
    
        validate function added
    
    debugging - sessid and refactoring

--- game/resources/game_resource.py
+++ game/resources/game_resource.py
@@ -8,86 +8,86 @@ from uuid import uuid4
 import time
 
 class GameResource(Resource):
-	def __init__(self):
-		self.in_queue = app.in_queue
-		self.out_queue = app.out_queue
-		self.queue_key = app.config['QUEUE_KEY']
-		self.lock = Lock()
+    def __init__(self):
+        self.in_queue = app.in_queue
+        self.out_queue = app.out_queue
+        self.queue_key = app.config['QUEUE_KEY']
+        self.lock = Lock()
 
-	def __set_value(self, sessid, message):
-		key = '{}:{}'.format(self.queue_key, sessid)
-		value = message
-		self.lock.acquire()
-		self.in_queue[sessid][key] = value
-		self.lock.release()
+    def __set_value(self, sessid, message):
+        key = '{}:{}'.format(self.queue_key, sessid)
+        value = message
+        self.lock.acquire()
+        self.in_queue[sessid][key] = value
+        self.lock.release()
 
-	def __get_value(self, sessid):
-		'''Retrives an entry from the queue '''
-		if not self.out_queue.get(sessid, None):
-			return -1, None
-		key = '{}:{}'.format(self.queue_key, sessid)
-		retry_count = 5
-		for _ in range(retry_count):
-			start = time.time()
-			value = self.out_queue[sessid].get(key, None)
-			end = time.time()
-			if value is not None:
-				status = 0
-				break
-			else:
-				time.sleep(1)
-				continue
-		if value is None:
-			status = -1
-		else:
-			self.out_queue[sessid] = {}
+    def __get_value(self, sessid):
+        '''Retrives an entry from the queue '''
+        if not self.out_queue.get(sessid, None):
+            return -1, None
+        key = '{}:{}'.format(self.queue_key, sessid)
+        retry_count = 5
+        for _ in range(retry_count):
+            start = time.time()
+            value = self.out_queue[sessid].get(key, None)
+            end = time.time()
+            if value is not None:
+                status = 0
+                break
+            else:
+                time.sleep(1)
+                continue
+        if value is None:
+            status = -1
+        else:
+            self.out_queue[sessid] = {}
 
-		return status, value
+        return status, value
 
-	def get(self):
-		'''Subsequent get requests from client side with sessid and response message returns the game state'''
-		parser = reqparse.RequestParser(bundle_errors = True)
-		parser.add_argument('sessid', required = False, type = str, default = 'default', location = 'headers', help = 'Id of the session.')
-		parser.add_argument('message', required = False, type = str, default = '', location = 'headers', help = 'Selection response from client.')
+    def get(self):
+        '''Subsequent get requests from client side with sessid and response message returns the game state'''
+        parser = reqparse.RequestParser(bundle_errors = True)
+        parser.add_argument('sessid', required = False, type = str, default = 'default', location = 'headers', help = 'Id of the session.')
+        parser.add_argument('message', required = False, type = str, default = '', location = 'headers', help = 'Selection response from client.')
 
-		args = parser.parse_args()
-		sessid = args.sessid
-		message = args.message
-		self.__set_value(sessid, message)
-		start_time = time.time()
-		status = -1
-		while status == -1:
-			status, result = self.__get_value(sessid)
-		end_time = time.time()
+        args = parser.parse_args()
+        sessid = args.sessid
+        message = args.message
+        self.__set_value(sessid, message)
+        start_time = time.time()
+        status = -1
+        while status == -1:
+            status, result = self.__get_value(sessid)
+        end_time = time.time()
 
-		if status != 0:
-			result = dict({'Error' : 'Entry not found!'})
+        if status != 0:
+            result = dict({'Error' : 'Entry not found!'})
 
-		response =  dict({'status' : status, 'data' : result, 'sessid' : sessid})
-		return response
+        response =  dict({'status' : status, 'data' : result, 'sessid' : sessid})
+        return response
 
-	def post(self):
-		'''Initial post request from client side starts a new game'''
-		parser = reqparse.RequestParser(bundle_errors = True)
-		parser.add_argument('sessid', required = False, type = str, location = 'headers', help = 'Id of the session.')
+    def post(self):
+        '''Initial post request from client side starts a new game'''
+        parser = reqparse.RequestParser(bundle_errors = True)
+        parser.add_argument('sessid', required = False, type = str, location = 'headers', help = 'Id of the session.')
 
-		args = parser.parse_args()
-		sessid = args.sessid
+        args = parser.parse_args()
+        sessid = args.sessid
 
-		if not sessid:
-			sessid = str(uuid4())
-		game_service = GameService(sessid)
-		status, result = game_service.init_game()
-		game_service.start()
-		result = None
-		if status == 0:
-			status = -1
-			while status == -1:
-				status, result = self.__get_value(sessid)
-			print (result)
-		if status == 0:
-			result = dict({'status' : status, 'data': result,  'sessid' : sessid})
-		else:
-			result = dict({'status' : status, 'error' : 'Game start failed!'})
+        if not sessid:
+            sessid = str(uuid4())
+        game_service = GameService(sessid)
+        status, result = game_service.init_game()
+        game_service.start()
+        result = None
+        if status == 0:
+            status = -1
+            while status == -1:
+                status, result = self.__get_value(sessid)
+            print (result)
+        if status == 0:
+            result = dict({'status' : status, 'data': result,  'sessid' : sessid})
+        else:
+            result = dict({'status' : status, 'error' : 'Game start failed!'})
 
-		return result
+        return result
--- game/routes.py
+++ game/routes.py
@@ -7,7 +7,7 @@ from flask import url_for
 from flask import request
 
 from game.app import app
-from game.utils import *
+from game.utilities import *
 
 
 @app.route('/')
@@ -23,6 +23,7 @@ def index():
                     "Your job is to ensure the growth of the platform to profitability.",
                     "Do this by managing the speed you grow the platform and choosing the best method of funding to minimize your company's Weighted Average Cost of Capital (WACC)."
                     ],
+        sessid   = getSessionId(),
         introHeader = "Hi There!!!!",
         introBlabel   = "Next >>",
         id       = 'gameStart'
@@ -30,8 +31,15 @@ def index():
 
 @app.route('/game',methods = ['GET','POST'])
 def gameStart():
+
+    print("\n\nOut-Queue:",app.out_queue)
+    print("\n\in-Queue:",app.in_queue)
+
     if request.method == 'POST':
-        return redirect('/main')
+        responseWelcome = send_post_request()
+        if validateResponse(responseWelcome):
+            return redirect(url_for('main',dataWelcome = str(responseWelcome[3:12])))
+        return redirect('/error')
     else:
         response = None
         response = request.args.get('main')
@@ -39,65 +47,98 @@ def gameStart():
 
         while response != 12:
             if response == 1:
-                return redirect('/hire-fire')
+                responseHireFire = send_get_request('1')
+                if validateResponse(responseHireFire):
+                    return redirect(url_for('hireFire', dataHireFire = str(responseHireFire[0])))
+                return redirect('/error')
 
             elif response == 2:
-                return redirect('/platform-income-statement')
+                responsePIS = send_get_request('2')
+                if validateResponse(responsePIS):
+                    return redirect(url_for('platformIncomeStatement', dataPIS = str(responsePIS[0:9])))
+                return redirect('/error')
 
             elif response == 3:
-                return redirect('/platform-balance-sheet')
+                responsePBS = send_get_request('3')
+                if validateResponse(responsePBS):
+                    return redirect(url_for('platformBalanceSheet', dataPBS = str(responsePBS[0:7])))
+                return redirect('/error')
 
             elif response == 4:
-                return redirect('/platform-cashflow-statement')
+                responsePCS = send_get_request('4')
+                if validateResponse(responsePCS):
+                    return redirect(url_for('platformCashflowStatement', dataPCS = str(responsePCS[:7])))
+                return redirect('/error')
 
             elif response == 5:
-                return redirect('/loan-performance')
+                responseLoanPerformance = send_get_request('5')
+                if validateResponse(responseLoanPerformance):
+                    return redirect(url_for('loanPerformance', dataLP = str(responseLoanPerformance[0:6])))
+                return redirect('/error')
 
             elif response == 6:
-                return redirect('/loan-buyer-cash')
+                responseLBC = send_get_request('6')
+                if validateResponse(responseLBC):
+                    return redirect(url_for('loanBuyerCash', dataLBC = str(responseLBC[0:3])))
+                return redirect('/error')
 
             elif response == 7:
-                return redirect('/sell-loans')
+                responseSellLoans = send_get_request('7')
+                if validateResponse(responseSellLoans):
+                    return redirect(url_for('sellLoans', dataSL = str(responseSellLoans[0:4])))
+                return redirect('/error')
 
             elif response == 8:
-                return redirect('/securitize-loans')
+                responseSecuritizeLoans =  send_get_request('8')
+                if validateResponse(responseSecuritizeLoans):
+                    return redirect(url_for('securitizeLoans', dataSecuritizeLoans = str(responseSecuritizeLoans[0:4])))
+                return redirect('/error')
 
             elif response == 9:
-                return redirect('/sell-into-credit-facility')
+                responseSellIntoCreditFacility =  send_get_request('9')
+                if validateResponse(responseSellIntoCreditFacility):
+                    return redirect(url_for('sellIntoCreditFacility', dataSICF = str(responseSellIntoCreditFacility[0:1])))
+                return redirect('/error')
 
             elif response == 10:
                 return "TODO: Wil be here soon."
 
             elif response == 11:
-                return redirect('/credit-facility-info')
+                responseCFI =  send_get_request('11')
+                if validateResponse(responseCFI):
+                    return redirect(url_for('creditFacilityInfo', dataCFI = str(responseCFI[0:4])))
+                return redirect('/error')
 
             elif response == 13:
                 return flask.redirect('/quit')
-        return redirect('/next-quarter')
 
+        responseNQ =  send_get_request('12')
+        if validateResponse(responseNQ):
+            return redirect(url_for('nextQuarter', dataNQ = str(responseNQ[0:7])))
+        return redirect('/error')
 
-@app.route('/game/1')
+@app.route('/game/hire-fire')
 def game_hire():
     responseHire = request.args.get('hire')
     responseMessageHire = send_get_request(responseHire)
     return game_loop(header = "Bingo,Some HR stuff has been done.", messages = ["Let's continue the pace."])
 
 
-@app.route('/game/7')
+@app.route('/game/sell-loans')
 def game_sell():
     responseSell = request.args.get('sell')
     responseMessageSell = send_get_request(responseSell)
     return game_loop("Transaction successfully completed.Cheers :)",['So good so far.'])
 
 
-@app.route('/game/8')
+@app.route('/game/securitize-loans')
 def game_perform_securitization():
     responseSecure = request.args.get('secure')
     responseMessageSecure = send_get_request(responseSecure)
     return game_loop("You've done it.",['"Beware of little expenses. A small leak will sink a great ship."\n- B.Frank', "Go on."])
 
 
-@app.route('/game/9')
+@app.route('/game/sell-into-credit-facility')
 def game_perform_credit_sell():
     responseSellFacility = request.args.get('sellFacility')
     responseMessageSellFacility = send_get_request(responseSellFacility)
@@ -112,118 +153,100 @@ def restart():
 
 @app.route('/error')
 def error():
+
     return render_template(
         'error.djhtml',
         header   = 'Uh oh!!! :(',
         messages = ['Sorry, Something went wrong.','Please restart the game.'],
         blabel   = "Why not??",
-        id       = "gameStart"
+        id       = "gameStart",
+        sessid   = getSessionId()
         )
 
 
-@app.route('/hire-fire')
-def hireFire():
-        responseHireFire = send_get_request('1')
-        if not responseHireFire:
-            return redirect('/error')
+@app.route('/hire-fire/<dataHireFire>')
+def hireFire(dataHireFire):
         return flask.render_template(
             'hireFire.djhtml',
             header     = "“Great vision without great people is irrelevant.",
             messages   = ["How many underwriters would you like to hire or fire, choose suitably??"],
-            stats      = [responseHireFire[0]],
+            stats      = [dataHireFire],
             question   = "The moment has come.",
-            uri        = 'game/1',
+            uri        = '/game/hire-fire',
             field_name = 'hire',
-            sessid     = flask.session['sessionData']['sessid']
+            sessid     = getSessionId()
         )
 
 
-@app.route('/platform-income-statement')
-def platformIncomeStatement():
-    responsePIS = send_get_request('2')
-    if not responsePIS:
-        return redirect('/error')
+@app.route('/platform-income-statement/<dataPIS>')
+def platformIncomeStatement(dataPIS):
     return flask.render_template(
         'game.djhtml',
         header     = "Platform Income Statement",
-        stats      = responsePIS[0:9],
+        stats      = eval(dataPIS),
         messages   = ["Go on, what would you like to do now??"],
-        sessid     = flask.session['sessionData']['sessid'],
+        sessid     = getSessionId() ,
         **gameLoopQuestions()
     )
 
 
-@app.route('/platform-balance-sheet')
-def platformBalanceSheet():
-    responsePBS = send_get_request('3')
-    if not responsePBS:
-        return redirect('/error')
+@app.route('/platform-balance-sheet/<dataPBS>')
+def platformBalanceSheet(dataPBS):
     return flask.render_template(
         'game.djhtml',
         header   = "Platform Balance Sheet.",
-        sessid     = flask.session['sessionData']['sessid'],
-        stats    = responsePBS[0:7],
+        sessid     = getSessionId() ,
+        stats    = eval(dataPBS),
         messages = ["Keep up the good business.What next??"],
         **gameLoopQuestions()
     )
 
 
-@app.route('/platform-cashflow-statement')
-def platformCashflowStatement():
-    responsePCS = send_get_request('4')
-    if not responsePCS:
-        return redirect('/error')
+@app.route('/platform-cashflow-statement/<dataPCS>')
+def platformCashflowStatement(dataPCS):
     return flask.render_template(
         'game.djhtml',
         header   = "Platform Cashflow statement.",
-        stats    = responsePCS[0:7],
-        sessid     = flask.session['sessionData']['sessid'],
+        stats    = eval(dataPCS),
+        sessid     = getSessionId() ,
         messages = ["What will be your next command??"],
         **gameLoopQuestions()
     )
 
 
-@app.route('/loan-performance')
-def loanPerformance():
-    responseLoanPerformance = send_get_request('5')
-    if not responseLoanPerformance:
-        return redirect('/error')
+@app.route('/loan-performance/<dataLP>')
+def loanPerformance(dataLP):
     return flask.render_template(
         'game.djhtml',
         header   = "Loan Performance",
-        stats    = responseLoanPerformance[0:6],
-        sessid   = flask.session['sessionData']['sessid'],
+        stats    = eval(dataLP),
+        sessid   = getSessionId() ,
         messages = ["What would you like to do next ??"],
         **gameLoopQuestions()
     )
 
 
-@app.route('/loan-buyer-cash')
-def loanBuyerCash():
-    responseLBC = send_get_request('6')
-    if not responseLBC:
-        return redirect('/error')
+@app.route('/loan-buyer-cash/<dataLBC>')
+def loanBuyerCash(dataLBC):
     return flask.render_template(
         'game.djhtml',
         header   = "Loan buyer cash.",
-        stats    = responseLBC[0:3],
+        stats    = eval(dataLBC),
         messages = ["What could be the next step to success??"],
-        sessid     = flask.session['sessionData']['sessid'],
+        sessid     = getSessionId() ,
         **gameLoopQuestions()
     )
 
 
-@app.route('/sell-loans')
-def sellLoans():
-    responseSellLoans = send_get_request('7')
-    if not responseSellLoans:
-        return redirect('/error')
+@app.route('/sell-loans/<dataSL>')
+def sellLoans(dataSL):
+    responseSellLoans = eval(dataSL)
     purchased = responseSellLoans[0]
     if purchased:
         return flask.render_template(
             'game.djhtml',
             header   = "Oops!!",
-            sessid     = flask.session['sessionData']['sessid'],
+            sessid     = getSessionId() ,
             stats    = ["Buyer A already purchased. Please choose another option."],
             messages = ["What would you like to do next ??"],
             **gameLoopQuestions()
@@ -231,58 +254,46 @@ def sellLoans():
     return flask.render_template(
         'sellLoans.djhtml',
         header     = "Let's sell some Loans.",
-        uri        = 'game/7',
-        sessid     = flask.session['sessionData']['sessid'],
+        uri        = '/game/sell-loans',
+        sessid     = getSessionId(),
         question   = "How much would you like to sell (enter 0 to not sell)?",
         warning    = responseSellLoans[1],
         limit      = responseSellLoans[2],
         field_name = "sell"
     )
 
-@app.route('/securitize-loans')
-def securitizeLoans():
-    responseSecuritizeLoans =  send_get_request('8')
-    if not responseSecuritizeLoans:
-        return redirect('/error')
-
+@app.route('/securitize-loans/<dataSecuritizeLoans>')
+def securitizeLoans(dataSecuritizeLoans):
     return flask.render_template(
         'securitizeLoans.djhtml',
         header     = "Let's Securitize some Loans.",
-        uri        = 'game/8',
-        sessid     = flask.session['sessionData']['sessid'],
-        info       = responseSecuritizeLoans[0:3],
+        uri        = '/game/securitize-loans',
+        sessid     = getSessionId(),
+        info       = eval(dataSecuritizeLoans),
         question   = "How large would you like buyer D's tranche to be (enter 0 to not sell)?",
-        limit      = responseSecuritizeLoans[3],
         field_name = "secure"
     )
 
-@app.route('/sell-into-credit-facility')
-def sellIntoCreditFacility():
-    responseSellIntoCreditFacility =  send_get_request('9')
-    if not responseSellIntoCreditFacility:
-        return redirect('/error')
+@app.route('/sell-into-credit-facility/<dataSICF>')
+def sellIntoCreditFacility(dataSICF):
     return flask.render_template(
         'securitizeLoans.djhtml',
         header     = "Sell into credit facility.",
-        uri        = 'game/9',
-        sessid     = flask.session['sessionData']['sessid'],
-        info       = [responseSellIntoCreditFacility[0]],
+        uri        = '/game/sell-into-credit-facility',
+        sessid     = getSessionId(),
         question   = "How much would you like to sell (enter 0 to not sell)?",
-        limit      = responseSellIntoCreditFacility[1],
+        info      = eval(dataSICF),
         field_name = 'sellFacility'
     )
 
 
-@app.route('/credit-facility-info')
-def creditFacilityInfo():
-    responseCFI =  send_get_request('11')
-    if not responseCFI:
-        return redirect('/error')
+@app.route('/credit-facility-info/<dataCFI>')
+def creditFacilityInfo(dataCFI):
     return flask.render_template(
         'game.djhtml',
         header   = "Credit Facility Info",
-        sessid     = flask.session['sessionData']['sessid'],
-        stats    = responseCFI[0:4],
+        sessid     = getSessionId(),
+        stats    = eval(dataCFI),
         messages = ["What would you like to do next ??"],
         **gameLoopQuestions()
     )
@@ -295,29 +306,23 @@ def quit():
     return redirect('/')
 
 
-@app.route('/main')
-def main():
-    responseWelcome = send_post_request()
-    if not responseWelcome:
-        return redirect('/restart')
+@app.route('/next-quarter/<dataNQ>')
+def nextQuarter(dataNQ):
     return render_template(
         'game.djhtml',
-        sessid     = flask.session['sessionData']['sessid'],
-        header     = "On your command.",
-        stats      = responseWelcome[3:12],
+        header     = "Congratulations.",
+        sessid     = getSessionId() ,
+        stats      = eval(dataNQ),
         **gameLoopQuestions()
     )
 
 
-@app.route('/next-quarter')
-def nextQuarter():
-    responseNextQuarter =  send_get_request('12')
-    if not responseNextQuarter:
-        return flask.redirect('/restart')
+@app.route('/main/<dataWelcome>')
+def main(dataWelcome):
     return render_template(
         'game.djhtml',
-        header     = "Congratulations.",
-        sessid     = flask.session['sessionData']['sessid'],
-        stats      = responseNextQuarter[0:7],
+        header     = "On your command.",
+        stats      = eval(dataWelcome),
+        sessid     = getSessionId(),
         **gameLoopQuestions()
     )
